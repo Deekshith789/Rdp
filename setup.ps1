a0a6abd7c191d64a59c3b77b726622df1e14749a
mrx7470 <122193222+mrx7470@users.noreply.github.com>
2023-01-15 17:08:41 +0530

    Create setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,3 +1,13 @@
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
 & {$P = $env:TEMP + '\chromeremotedesktophost.msi'; Invoke-WebRequest 'https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile $P; Start-Process $P -Wait; Remove-Item $P}
 & {$P = $env:TEMP + '\chrome_installer.exe'; Invoke-WebRequest 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile $P; Start-Process -FilePath $P -Args '/install' -Verb RunAs -Wait; Remove-Item $P}
+# Install Chocolatey package manager
+iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# Install required software packages
+choco install -y git
+choco install -y visualstudio2019buildtools
+choco install -y dotnetcore-sdk
+
+# Set environment variables
+$env:PATH += ";C:\ProgramData\chocolatey\bin"
